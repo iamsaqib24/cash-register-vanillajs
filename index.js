00a724587926f3c5c60a3f8b17e2e83314b04034
Saqib <iamsaqib24@gmail.com>
2022-10-26 18:03:17 +0530

    code wrote but not working

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-const billAmount = document.querySelector("#bill-amount");
-var cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
+const billAmount = document.querySelector('#bill-amount');
+const cashGiven = document.querySelector('#cash-given');
+const checkButton = document.querySelector('#check-button');
+const message = document.querySelector('#error-message');
+const notes = document.querySelectorAll(".note");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click", function v_B_and_C_Amount(){
-    message.style.display = "none";
+function hideMessage(msg) {
+    message.style.display = "";
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
+
+checkButton.addEventListener("click", function validateBillAndCash() {
+    hideMessage();
     if (billAmount.value > 0) {
-        if (cashGiven.value > billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculatorChange(amountToBeReturned);
-        }else{
-            showMessage("The cash provided should atleast be equal to the bill amount.");
+        if (cashGiven.value >= billAmount.value) {
+            const returnAmount = cashGiven.value - billAmount.value;
+            calculateChange(returnAmount);
         }
-    }else{
-        showMessage("Invalid bill amount");
+        else{
+            showMessage("Get ready to wash dishes!");
+        }
+    }
+    // }else if (billAmount.vaalue === String ) {
+    //     showMessage("Don't give string as input instead of number.");
+    // }
+    else{
+        showMessage("Invalid input");
     }
 });
 
-function calculatorChange(){
-
+function calculateChange(returnAmount) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(returnAmount / availableNotes[i]);
+        returnAmount %= availableNotes[i];
+        notes.innerText =numberOfNotes;
+    }
 }
-
-function showMessage(message) {
-    message.style.display = "block";
-    message.innerText = message;
-}
-// billAmount.addEventListener("cl")
